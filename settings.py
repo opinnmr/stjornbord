b69bbe83d338958a2e0a6d64ed241dec07c593b2
Bjorn Patrick Swift <bjorn@swift.is>
2013-12-12 22:24:59 -0800

    Switch to Django's UnsaltedSHA1PasswordHasher
    This is to support decrypting legacy passwords. Removing our old implementation (since Django now comes with one).

--- settings.py
+++ settings.py
@@ -54,7 +54,7 @@ PASSWORD_HASHERS = (
     'django.contrib.auth.hashers.PBKDF2PasswordHasher',
     'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
     'django.contrib.auth.hashers.BCryptPasswordHasher',
-    'stjornbord.user.auth.SaltlessSHA1PasswordHasher',
+    'django.contrib.auth.hashers.UnsaltedSHA1PasswordHasher', # Legacy
 )
 
 
